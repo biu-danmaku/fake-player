920851f370c6ffbb15b09ed9bf333d44816b7814
sdjdd <sdjddrt@gmail.com>
2019-02-12 14:13:29 +0800

    Add dependence

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-    mode: 'development',
-    output: {
-        filename: 'fake-player.min.js',
-        publicPath: '/dist'
-    },
+  mode: 'development',
+  output: {
+    filename: 'fake-player.min.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  }
 }
